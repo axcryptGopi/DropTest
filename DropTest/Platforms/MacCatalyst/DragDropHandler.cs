ba56da403392aeedbfd0e3e09f77b7fac21eaa9a
Gopinath <Gopinath.b@axcrypt.net>
2025-09-09 10:40:32 +0530

    test commit

---
+++ DropTest/Platforms/MacCatalyst/DragDropHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UIKit;
+
+namespace DropTest.Platforms.MacCatalyst
+{
+    public class DragDropHandler : UIDropInteractionDelegate
+
+    {
+        private UIDropInteraction Interaction;
+
+        public DragDropHandler(UIView view)
+        {
+            view.UserInteractionEnabled = true;
+            Interaction = new UIDropInteraction(this);
+            view.AddInteraction(Interaction);
+        }
+
+
+        public Boolean AllowDrop
+        {
+            get; set;
+        }
+
+        public override bool CanHandleSession(UIDropInteraction interaction, IUIDropSession session)
+        {
+            Console.WriteLine("CanHandleSession fired " + AllowDrop);
+            return AllowDrop;
+        }
+        public override void SessionDidEnter(UIDropInteraction interaction, IUIDropSession session)
+        {
+            base.SessionDidEnter(interaction, session);
+        }
+        public override UIDropProposal SessionDidUpdate(UIDropInteraction interaction, IUIDropSession session)
+        {
+            Console.WriteLine("Session Did Update fired");
+            return new UIDropProposal(UIDropOperation.Copy);
+        }
+
+        public override void PerformDrop(UIDropInteraction interaction, IUIDropSession session)
+        {
+            Console.WriteLine("PerformDrop fired");
+        }
+    }
+
+}
---
+++ DropTest/obj/Debug/net9.0-android/android/src/crc64f0e5f81739b13bf5/MainActivity.java
@@ -0,0 +1,46 @@
+package crc64f0e5f81739b13bf5;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("DropTest.MainActivity, DropTest", MainActivity.class, __md_methods);
+	}
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("DropTest.MainActivity, DropTest", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("DropTest.MainActivity, DropTest", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
