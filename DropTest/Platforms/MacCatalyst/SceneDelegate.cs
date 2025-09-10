cafe2ea459213088796b2c29d878f99ee6d16eb3
Gopinath <Gopinath.b@axcrypt.net>
2025-09-10 12:23:47 +0530

    test

---
+++ DropTest/Platforms/MacCatalyst/SceneDelegate.cs
@@ -0,0 +1,15 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropTest.Platforms.MacCatalyst
+{
+    [Register("SceneDelegate")]
+    public class SceneDelegate : MauiUISceneDelegate
+    {
+    }
+
+}
