9a3d1838b066fc762265d567cd7e224e4f40d126
Gopinath <Gopinath.b@axcrypt.net>
2025-10-17 15:26:09 +0530

    cheanges

--- DropTest/MainPage.xaml.cs
+++ DropTest/MainPage.xaml.cs
@@ -2,56 +2,66 @@
 {
     public partial class MainPage : ContentPage
     {
+#if MACCATALYST
+        private DropTest.Platforms.MacCatalyst.DragDropHandler handle;
+#endif
         public MainPage()
         {
             InitializeComponent();
 
             this.Loaded += MainPage_Loaded;
-//            Microsoft.Maui.Handlers.PageHandler.Mapper.AppendToMapping("DragDrop", (handler, view) =>
-//            {
+            Microsoft.Maui.Handlers.PageHandler.Mapper.AppendToMapping("DragDrop", (handler, view) =>
+            {
 
-//#if WINDOWS
-//                Microsoft.Maui.Platform.ContentPanel nativeView = handler.PlatformView;
-//                nativeView.AllowDrop = true;
-//                nativeView.DragOver += (sender, e) =>
-//                {
-//                    e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Move;
-//                    e.Handled = true;
-//                };
+#if WINDOWS
+                Microsoft.Maui.Platform.ContentPanel nativeView = handler.PlatformView;
+                nativeView.AllowDrop = true;
+                nativeView.DragOver += (sender, e) =>
+                {
+                    e.AcceptedOperation = Windows.ApplicationModel.DataTransfer.DataPackageOperation.Move;
+                    e.Handled = true;
+                };
 
-//                nativeView.Drop += async (sender, e) =>
-//                {
-//                    if (e.DataView.Contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.StorageItems))
-//                    {
-//                        var items = await e.DataView.GetStorageItemsAsync();
-//                        foreach (var item in items)
-//                        {
-//                            if (item is Windows.Storage.StorageFile file)
-//                            {
-//                                // Handle your file here (e.g., get path, open, etc.)
-//                                string path = file.Path;
-//                                System.Diagnostics.Debug.WriteLine($"Dropped file: {path}");
-//                            }
-//                        }
-//                    }
-//                    e.Handled = true;
-//                };
+                nativeView.Drop += async (sender, e) =>
+                {
+                    if (e.DataView.Contains(Windows.ApplicationModel.DataTransfer.StandardDataFormats.StorageItems))
+                    {
+                        var items = await e.DataView.GetStorageItemsAsync();
+                        foreach (var item in items)
+                        {
+                            if (item is Windows.Storage.StorageFile file)
+                            {
+                                // Handle your file here (e.g., get path, open, etc.)
+                                string path = file.Path;
+                                System.Diagnostics.Debug.WriteLine($"Dropped file: {path}");
+                            }
+                        }
+                    }
+                    e.Handled = true;
+                };
 
-//#elif MACCATALYST
-//                Microsoft.Maui.Platform.ContentView nativeView = handler.PlatformView;
+#elif MACCATALYST
+                //Microsoft.Maui.Platform.ContentView nativeView = handler.PlatformView;
 
-//                DropTest.Platforms.MacCatalyst.DragDropHandler handle = new DropTest.Platforms.MacCatalyst.DragDropHandler(nativeView);
-//                handle.AllowDrop = true;
-//#endif
+                //DropTest.Platforms.MacCatalyst.DragDropHandler handle = new DropTest.Platforms.MacCatalyst.DragDropHandler(nativeView);
+                //handle.AllowDrop = true;
+#endif
 
-//            });
+            });
         }
 
         private void MainPage_Loaded(object? sender, EventArgs e)
         {
-            DropGestureRecognizer dropGR = new DropGestureRecognizer();
-            dropGR.Drop += DropGR_Drop;
-            this.blazorWebView.GestureRecognizers.Add(dropGR);
+            //DropGestureRecognizer dropGR = new DropGestureRecognizer();
+            //dropGR.Drop += DropGR_Drop;
+            //this.blazorWebView.GestureRecognizers.Add(dropGR);
+
+
+
+#if MACCATALYST
+            DropTest.Platforms.MacCatalyst.DragDropHandler handle = new DropTest.Platforms.MacCatalyst.DragDropHandler(this.blazorWebView.Handler.PlatformView as UIKit.UIView);
+            handle.AllowDrop = true;
+#endif
         }
 
         private void DropGR_Drop(object? sender, DropEventArgs e)
